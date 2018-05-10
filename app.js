029b64b3c7810e48c4809d696bb2e95e3e9a6881
Chris Gonzales <cm.gonzales@yahoo.com>
2018-05-09 19:12:13 -0700

    intalled mongoose

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 var path = require('path');
 
 const app = express();
+
+//Map global promose - get rid of warning
+mongoose.Promise = global.Promise;
+
+
+//Connecing to mongoose
+mongoose.connect('mongodb://localhost/diary-dev', {
+useMongoClient: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
 
 const port = 4000;
 
